b96e8dcbbfe4cce11bb89745ac74bab08cfb4cb0
LamNguyen <lamnt.8x@gmail.com>
2023-04-03 17:18:47 +0700

    Fixed about container and rebuild for production

---
+++ .next/server/chunks/900.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.id = 900;
+exports.ids = [900];
+exports.modules = {
+
+/***/ 2900:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/netflix.89431ced.jpg","height":1390,"width":2000,"blurDataURL":"data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoKCgoKCgsMDAsPEA4QDxYUExMUFiIYGhgaGCIzICUgICUgMy03LCksNy1RQDg4QFFeT0pPXnFlZXGPiI+7u/sBCgoKCgoKCwwMCw8QDhAPFhQTExQWIhgaGBoYIjMgJSAgJSAzLTcsKSw3LVFAODhAUV5PSk9ecWVlcY+Ij7u7+//CABEIAAYACAMBIgACEQEDEQH/xAAoAAEBAAAAAAAAAAAAAAAAAAAABgEBAQAAAAAAAAAAAAAAAAAAAwT/2gAMAwEAAhADEAAAAJwHT//EABwQAQEAAQUBAAAAAAAAAAAAAAECAwAFEiExQf/aAAgBAQABPwA26chmIyPc1UDJIM+jx+a//8QAGREAAQUAAAAAAAAAAAAAAAAAAQACEjGR/9oACAECAQE/AJENFYv/xAAXEQEAAwAAAAAAAAAAAAAAAAABABEx/9oACAEDAQE/AMaFn//Z","blurWidth":8,"blurHeight":6});
+
+/***/ })
+
+};
+;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'nextjs-portfolio-ocumlmufc-lamnt8x-gmailcom.vercel.app/',
+    ]
+  }
 }
 
 module.exports = nextConfig
